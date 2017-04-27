3f2f33cd406f1c430da7d9d7c6310dec2135f55d
Gerardo Fang <Jerry@Gerardos-MacBook-Pro.local>
2017-04-26 18:41:38 -0600

    commit

--- backend/code/jerry/ProtocolIdentifier.py
+++ backend/code/jerry/ProtocolIdentifier.py
@@ -9,8 +9,36 @@
 """ Contract 20: Scan PDML """
 # @requires file type is of PDML and has been opened by PFS
 # @ensures returns a list of all identified protocols from the file
-def identifyProtocols(pdml):
-    """ Receives a file and returns a list of all identified protocols from it """
+import xml.etree.ElementTree as ET
+
+class ProtocolIdentifier:
+
+    def __init__(self, file):
+        self.listOfProtocols = []
+        self.file = file
+
+    def identifyProtocols(self):
+        Fh = open(self.file,'r')
+        tree = ET.parse(self.file)
+        root = tree.getroot()
+        for proto in root.iter('proto'):
+            name = proto.get('name')
+            self.listOfProtocols.append(name)
+        self.removeDuplicates()
+
+    def printList(self):
+        print (self.listOfProtocols)
+
+    def removeDuplicates(self):
+        newList = []
+        for i in self.listOfProtocols:
+            if i not in newList:
+                newList.append(i)
+        self.listOfProtocols = newList
+
+object1 = ProtocolIdentifier('nameOfPDML.pdml')
+object1.identifyProtocols()
+object1.printList()
 	
 
 # @requires file type is of PDML and has been opened by PFS
