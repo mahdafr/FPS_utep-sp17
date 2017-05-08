066c5e24359412ec515697f5e505f1270c0a1996
Mahdokht Af <mmafravi@gmail.com>
2017-05-07 23:12:37 -0600

    backend integrated (untested) and frontend was Juan's shenanigans

--- backend/code/integration/Filter.py
+++ backend/code/integration/Filter.py
@@ -1,39 +1,65 @@
 # -*- coding: utf-8 -*-
 """
-@author Mahdokht Afravi
+@author Gerardo Fang
 @created 04-18 T
-
-
-@modified mafravi
-
-
+@modified 05-07 U gfang, mafravi
 """
+import Action 
+import Expression
+#Contains Expression(s) by which to visualize and modify the PDML capture
+
+
 class Filter():
     expression = [] #Expression: contains the expression for this Filter
+    action = [] #Actions: contains the actions for this Filter
     
     """ Contract 8: Manage Filter """
+    
     # @requires the rule exists
     # @ensures 
-    def addFilter(rule,filter):
+    def addFilter(self,expr: Expression):
+        self.expression = expr
         """ Receives a filter and adds the filter to the rule """
         
     
     # @requires the filter is the last added to the rule
     # @ensures 
-    def deleteFilter(filter):
+    def deleteFilter(self):
         """ Deletes the filter """
+        self.expression = []
+    
+    # @requires 
+    # @ensures 
+    def addAction(self,actn: Action):
+        """ Appends this Action to the list of Actions for this Filter """
+        self.action.append(actn)
         
+    # @requires  List of Actions is not empty
+    # @ensures
+    def deleteAction(self):
+        """Deletes the last Action added to this Filter"""
+        del self.actions[-1]
     
     """ Contract 9: Manage Expression """
-    # @requires the expression is in the Berkely Packet Filter (BPF) syntax
-    # @ensures 
-    def addPrimitive(primitive,expression):
-        """ Receives a primitive and adds it to the expression """
-        
     
-    # @requires the primitive is contained in this expression
-    # @ensures 
-    def removePrimitive(primitive,expression):
-        """ Sets the new path to this script """
+    # @requires The Expression is in the Berkeley Packet Filter (BPF) syntax.
+    # @ensures The addition of a primitive to the Expression for this Filter.
+    def addToExpression(self,expr):
+        """Receives a primitive (composed from the string) and adds it to the Expression in this Filter"""
+        try: self.expression.append(expr)
+        except ValueError:
+            pass #handle if not in list (if needed)
+        except AttributeError:
+            pass #handle if list not a list (if needed)
+
+    # @requires
+    # @ensures
+    def deleteExpression(self, expr):
+        """Deletes the specified Expression from this Filter"""
+        try: self.expression.remove(expr)
+        except ValueError:
+            pass #handle if not in list (if needed)
+        except AttributeError:
+            pass #handle if list not a list (if needed)
         
     pass
